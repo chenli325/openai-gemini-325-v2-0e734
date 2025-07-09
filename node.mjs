883c4a4c06bacc077282c5e7e52822d63588dd98
chenli325 <31625326+chenli325@users.noreply.github.com>
2025-07-09 15:40:58 +0800

    Update node.mjs

--- node.mjs
+++ node.mjs
@@ -5,7 +5,12 @@ import worker from "./src/worker.mjs";
 const port = +(process.env.PORT || 8080);
 
 const serverAdapter = createServerAdapter(worker.fetch)
-const server = createServer(serverAdapter)
+const server = createServer({
+  // 禁用压缩
+  maxHeaderSize: 32768,  // 32KB
+  insecureHTTPParser: false,
+  noDelay: true,
+}, serverAdapter)
 server.listen(port, () => {
   console.log('Listening on:', server.address());
 })
